7632ba83f23b877c72e7047f9a2f6c86352ba70a
ivensgustavo <gustavo_ivens@alu.ufc.br>
2020-07-20 17:13:14 -0300

    challenge finished

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,63 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
 
+import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  categoryTitle: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    categoryTitle,
+  }: RequestDTO): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoriesRepository = getRepository(Category);
+
+    const { total } = await transactionsRepository.getBalance();
+
+    if (type !== 'income' && type !== 'outcome') {
+      throw new AppError('This type is invalid.');
+    }
+
+    if (type === 'outcome' && value > total) {
+      throw new AppError(
+        "You don't have enough balance to complete the transaction.",
+      );
+    }
+
+    let findedCategory = await categoriesRepository.findOne({
+      where: {
+        title: categoryTitle,
+      },
+    });
+
+    if (!findedCategory) {
+      findedCategory = await categoriesRepository.create({
+        title: categoryTitle,
+      });
+
+      await categoriesRepository.save(findedCategory);
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category_id: findedCategory.id,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,118 @@
+import path from 'path';
+import csvParse from 'csv-parse';
+import fs from 'fs';
+import { getRepository, getCustomRepository, In } from 'typeorm';
 import Transaction from '../models/Transaction';
+import uploadConfig from '../config/upload';
+import Category from '../models/Category';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface RequestDTO {
+  fileName: string;
+}
+
+interface ImportedTransaction {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
+
+/*
+
+  Pra importar cada transaction é preciso ler cada linha do arquivo e criar uma transaction.
+  A vírgula delimita uma informação da transaction;
+  csv-parse é uma biblioteva para manipular arquivos csv.
+
+  O fs é uma biblioteca nativa do Node que ajuda a abrir, ler arquivos...
+
+*/
 
 class ImportTransactionsService {
-  async execute(): Promise<Transaction[]> {
-    // TODO
+  async execute({ fileName }: RequestDTO): Promise<Transaction[]> {
+    const filePath = path.join(uploadConfig.folder, fileName);
+
+    const categoriesRepository = getRepository(Category);
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    // cria um objeto de leitura
+    const readStream = fs.createReadStream(filePath);
+
+    const transactions: ImportedTransaction[] = [];
+    const categories: string[] = [];
+
+    // Crio a instância do csv-parse dizendo como vai ocorrer a tranformação
+    // Por padrão o delimitador é a vírgula
+    const parser = csvParse({
+      from_line: 2,
+    });
+
+    // ler enquanto tiver linhas disponíveis
+    const lines = readStream.pipe(parser);
+
+    lines.on('data', async line => {
+      const [title, type, value, category] = line.map((cell: string) => {
+        return cell.trim();
+      });
+
+      if (!title || !type || !value || !category) return;
+
+      categories.push(category);
+      transactions.push({ title, type, value, category });
+    });
+
+    await new Promise(resolve => parser.on('end', resolve));
+
+    // Book insert é inserir um conjunto de dados de uma vez pra não ficar
+    // abrindo e fechando conexão pois assim se perde em desempenho
+    // O método in vai verificar se um conjunto de categorias existe no bd
+
+    // Pego as categorias que já existem no bd
+    const existentCategories = await categoriesRepository.find({
+      where: {
+        title: In(categories),
+      },
+    });
+
+    // Pego apenas os títulos das categorias que já existem
+    const existentCategoriesTitles = existentCategories.map(
+      category => category.title,
+    );
+
+    // A partir das categorias que recebi no arquivo vejo quais delas não existem no bd
+    const nonExistentCategoriesTitles = categories
+      .filter(category => !existentCategoriesTitles.includes(category))
+      .filter((value, index, self) => self.indexOf(value) === index);
+
+    // Adicione as categorias que não existem no banco
+    const newCategories = await categoriesRepository.create(
+      nonExistentCategoriesTitles.map(title => ({
+        title,
+      })),
+    );
+
+    await categoriesRepository.save(newCategories);
+
+    // Junto todas as categorias
+    const finalCategories = [...existentCategories, ...newCategories];
+
+    // Salvo as transações no banco de uma só vez
+    const createdTransactions = await transactionsRepository.create(
+      transactions.map(transaction => ({
+        title: transaction.title,
+        type: transaction.type,
+        value: transaction.value,
+        category: finalCategories.find(
+          category => category.title === transaction.category,
+        ),
+      })),
+    );
+
+    await transactionsRepository.save(createdTransactions);
+
+    await fs.promises.unlink(filePath);
+
+    return createdTransactions;
   }
 }
 
